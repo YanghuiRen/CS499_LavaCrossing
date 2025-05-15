21ab726187da4de9f0fb7b13b4923020215a6b67
MDorn03 <medorn03@gmail.com>
2025-05-15 14:55:48 -0700

    Update q-learning-v2.py

--- src/q_learning/q-learning-v2.py
+++ src/q_learning/q-learning-v2.py
@@ -1,21 +1,99 @@
-import math
 import numpy
 import matplot.pyplot as plt
-import minigrid
 import gymnasium as gym
+import random
+import encode_state
 
-from minigrid.wrappers import SymbolicObsWrapper
 from minigrid.envs import crossing
+from minigrid.wrappers import SymbolicObsWrapper
+from minigrid.wrappers import ImgObsWrapper
+
+MAX_TRIALS = 50
+N_EPISODES = 100
+MAX_STEPS = 100
+NUM_ACTIONS = 3
+
+GAMMA = 0.975
+ALPHA = 0.5
+EPSILON = 0.3
 
 env = gym.make("MiniGrid-LavaCrossingS11N5-v0")
+env = ImgObsWrapper(env)
 
+# Action Space Representation
+# 0 - Left : Turn Left
+# 1 - Right : Turn Right
+# 2 - Forward: Move Forward 
 
-# Initialize Q(s, a), ∀s ∈ S, a ∈ A(s), arbitrarily, and Q(terminal-state, ·) = 0
-# Repeat (for each episode):
-# Initialize S
-# Repeat (for each step of episode):
-# Choose A from S using policy derived from Q (e.g., epsilon-greedy)
-# Take action A, observe R, S′
-# Q(S, A) ← Q(S, A) + α[R + γ maxa Q(S′, a) − Q(S, A)]
-# S ← S′;
-# until S is terminal
+Q = {} #Stores q-vals
+rewards_log = []
+steps_log = []
+
+def qlearning(): 
+    #Init Q(s,a) = 0
+    #Init Q(terminal state, *) = 0
+
+    for episode in range(N_EPISODES): 
+        obs, _ = env.reset()
+        current_state = encode_state(obs)
+
+        episode_reward = 0 # Reward accumulated during the episode
+        episode_steps = 0 # Steps for the current episode
+
+        for s in range(MAX_STEPS): 
+            current_state_hash = env.hash(current_state)
+
+            # Select an action derived from epsilon greedy policy
+            if random.random() < EPSILON: 
+                action = random.randint(0, 1-NUM_ACTIONS)
+            else: 
+                try: 
+                    action = numpy.argmax(Q[current_state_hash])
+                except KeyError: 
+                    Q[current_state_hash] = numpy.zeros(NUM_ACTIONS)
+                    action = numpy.argmax(Q[current_state_hash])
+
+            # Have the agent take the step and gather the outputs
+            next_obs, reward, terminated, truncated, res = env.step(action)
+
+            # Use the next observation to get the next state (s')
+            next_state = encode_state(next_obs)
+            next_state_hash = env.hash(next_state)
+
+            # Q-Value update
+            if current_state_hash not in Q: 
+                Q[current_state_hash] = random.randint(0, 1-NUM_ACTIONS)
+            if next_state_hash not in Q: 
+                Q[next_state_hash] = random.randint(0, 1-NUM_ACTIONS)
+
+            Q[current_state_hash][action] += ALPHA * (reward + GAMMA + numpy.argmax(Q[next_state_hash] - Q[current_state_hash][action]))
+    
+            # Update the reward values for this episode
+            episode_reward += reward
+            episode_steps += 1
+
+            if terminated: 
+                steps_log[episode] = episode_steps
+                rewards_log[episode] = episode_reward
+                print("Agent successfully completed the episode.")
+                break
+
+            if truncated: 
+                steps_log[episode] = episode_steps
+                rewards_log[episode] = 0
+                print("Agent did not successfully complete the episode.")
+                break
+
+            # Episode is still going, get ready for the next step
+            current_state = next_state
+
+    #For episode in episodes:
+        # Init S
+        #For step in episode:
+            #Choose A from S using policy derived from Q (episolon greedy)
+            #Take action A, observe R and S'
+            #Q(S, A) ← Q(S, A) + α[R + γ maxa Q(S′, a) − Q(S, A)]
+            #S ← S'
+            #Until S is terminal
+
+    return Q, steps_log, rewards_log
