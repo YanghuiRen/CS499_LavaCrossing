10a55584145aa62d877d3aefd7bed2f982eb89e8
Brandyn Tucknott <tucknotb@oregonstate.edu>
2025-05-17 17:11:29 -0700

    Add Q-learning-lambda (untested).

--- src/qll.py
+++ src/qll.py
@@ -1,7 +1,59 @@
-import header
+import gymnasium as gym
+import numpy as np
+import minigrid
+import matplotlib as plt
+import const
 
+from collections import defaultdict
+
+from minigrid.wrappers import SymbolicObsWrapper
+from minigrid.env import crossing
+from minigrid.core.constants import OBJECT_TO_IDX
+
+MAX_TRIALS = 50
+N_EPISODES = 100
+MAX_STEPS = 100
+
+GAMMA = 0.975
+ALPHA = 0.5
+EPSILON = 0.3
+LAMBDA = 0.9
+
+NUM_ACTIONS = 3
+
+# used for plotting graphs
+steps_log = []
+rewards_log = []
+
+
+def encode_state(obs):
+    """
+    encode_state(obs):
+        return tuple, hashtable for Q-table;
+    """
+    image = obs['image'] # should return shape (S, S, 3)
+    agent_idx = OBJECT_TO_IDX['agent']
+    goal_idx = OBJECT_TO_IDX['goal']
+    lava_idx = OBJECT_TO_IDX['lava']
+
+    # find agent pos:
+    agent_pos = tuple(int(x) for x in np.argwhere(image[:,:,2] == agent_idx)[0])
+
+    # agent direction:
+    agent_dir = int(obs['direction']) # 0, 1, 2, 3
+
+    # goal position:
+    goal_pos = tuple(int(x) for x in np.argwhere(image[:,:,2] == goal_idx)[0])
+
+    # lava distribution:
+    lava_pos_list = [tuple(int(x) for x in pos) for pos in np.argwhere(image[:,:,2] == lava_idx)]
+    lava_pos_tuple = tuple(sorted(lava_pos_list))
+
+    return (agent_pos, agent_dir, goal_pos, lava_pos_tuple)
 
 """
+Q-learning(lambda). Assume the passed in environment is a lava crossing env with a SymbolicObsWrapper.
+
 Initialize Q(s, a) arbitrarily and e(s, a) = 0 for all s,a
 Repeat for each episode:
     Initialize s, a
@@ -16,32 +68,102 @@ Repeat for each episode:
             If a' = a* then e(s, a) <-- gamma * lambda * e(s, a)
             else e(s, a) = 0
     until s is terminal
+
+==============================
+===== Definitions ============
+==============================
+current_state_hash := s
+current_action := a
+next_state := s'
+next_action := a'
+
 """
-def qlearn_lambda():
-    ...
-
-
-
-
-
-
-
-
-
-
-def main():
-    """
-    A little bit of testing to ensure that minigrid works. Can be deleted
-    """
-    env = header.gym.make("MiniGrid-LavaCrossingS11N5-v0", render_mode='human')
-    obs, _ = env.reset()
-    print(obs['image'].shape)
+def qlearn_lambda(env):
+    Q = defaultdict(lambda: np.zeros(NUM_ACTIONS))
+    E = defaultdict(lambda: np.zeros(NUM_ACTIONS))
     
-    done = False
-    while not done:
-        env.render()
-        action = env.action_space.sample()
-        obs, r, done, truncated, info = env.step(action)
+    for ep_idx in range(N_EPISODES):
+        # init s, a
+        obs, _ = env.reset()
+        current_state = encode_state(obs)
+        current_action = np.random.randint(NUM_ACTIONS)
+        
+        # needed for plotting
+        episode_reward = 0
+        episode_steps = 0
+        
+        
+        # for each step in the episode
+        for _ in range(MAX_STEPS):
+            # choose a' from s' using policy Q (eps-greedy)
+            current_state_hash = env.hash(current_state)
+            next_action = None
+            
+            if np.random.rand() < EPSILON:
+                next_action =  np.random.randint(NUM_ACTIONS)
+            else:
+                try:
+                    next_action = np.argmax(Q[current_state_hash])
+                except:
+                    Q[current_state_hash] = np.zeros(NUM_ACTIONS)
+                    next_action = np.argmax(Q[current_state_hash])
+                    
+            # agent take an action
+            next_obs, reward, done, truncated, _ = env.step(next_action)
+            
+            # use next observation to get next state
+            next_state = encode_state(next_obs)
+            next_state_hash = env.hash(next_state)
+            
+            # compute a*, delta, INC e(s, a)
+            optimal_action = np.argmax(Q[next_state_hash])
+            if Q[next_state_hash][optimal_action] == Q[next_state_hash][next_action]: # if a' ties for the max, then a* <-- a'
+                optimal_action = next_action
+            delta = reward + GAMMA * Q[next_state_hash][optimal_action] - Q[current_state][current_action]
+            E[current_state_hash][current_action] += 1
+                
+            # Q-value update
+            # note indexes in Q, E should align
+            for idx in Q:
+                # watch here: 
+                # numpy broadcasting should ensure the indicies match, but may not work as intended
+                Q[idx] += ALPHA * delta * E[idx]
+                E[idx] *= LAMBDA * GAMMA if optimal_action == next_action else 0
+                
+            
+            # update reward values and log data for plotting
+            episode_reward += reward
+            episode_steps += 1
+            
+            if done:
+                steps_log[ep_idx] = episode_steps
+                rewards_log[ep_idx] = episode_reward
+                print("Agent successfully completed an episode.")
+                break
+            if truncated:
+                steps_log[ep_idx] = episode_steps
+                rewards_log = 0
+                print("Agent did not successfully complete an episode. ")
+                break
+            
+            # prep for next episode step
+            current_state = next_state
+            current_action = next_action
+            
+            # episode still in progress
+        # end of each step in episode loop
+        
+    return Q
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+    env = gym.make("MiniGrid-LavaCrossingS11N5-v0")
+    env = SymbolicObsWrapper(env)
+    obs, _ = env.reset(seed=42)
